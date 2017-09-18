685003a1cbe0904c114f925e0b34dc49a9d12bda
mstomel <jajalope372@gmail.com>
2017-09-18 08:50:15 -0400

    v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int pos = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter word one");
+        String word1 = input.next();
+        System.out.println("Please enter word two");
+        String word2 = input.next();
+
+        if (word1.length() != word2.length()){
+
+            System.out.println("Not anagrams");
+
+        }else{
+
+            for (int c = 0; c<word1.length(); c++){
+
+                if(word1.substring(c, c+1).equals(word2.substring(0, 1))){
+
+                    System.out.println(word1.substring(c, c+1));
+
+                }else {
+
+                    System.out.println("miss");
+                    pos++;
+
+                }
+
+            }
+
+        }
+
     }
 }
