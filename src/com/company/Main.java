decb07b76af6f7e7d2e7cb8393c33aca868efb84
jajalope <jajalope372@gmail.com>
2017-09-21 09:45:12 -0400

    v1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,36 +6,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int pos = 0;
+        int ana = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter word one");
         String word1 = input.next();
         System.out.println("Please enter word two");
         String word2 = input.next();
 
-        if (word1.length() != word2.length()){
+        if (word1.length() != word2.length()) {
 
             System.out.println("Not anagrams");
 
-        }else{
+        } else {
 
-            for (int c = 0; c<word1.length(); c++){
+            int l = word1.length();
+            for (int pos = 0; pos<l; pos++) {
+                for (int a = 0; a < word1.length(); a++) {
 
-                if(word1.substring(c, c+1).equals(word2.substring(0, 1))){
+                    if (word1.substring(a, a + 1).equals(word2.substring(pos, pos + 1))) {
 
-                    System.out.println(word1.substring(c, c+1));
+                        System.out.println(word1.substring(a, a + 1));
+                        ana++;
+                        break;
 
-                }else {
+                    } else {
 
-                    System.out.println("miss");
-                    pos++;
 
+
+                    }
 
                 }
+
+            }if (ana == l){
+
+                System.out.println("anagrams");
+
+            }else{
+
+                System.out.println("not anagrams");
 
             }
 
         }
-
     }
 }
